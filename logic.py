f41b95f8aae5f7f2af6768320a4b191286b3f811
ItWasMeqqq <dimanovd1@bk.ru>
2025-12-13 11:04:50 +0300

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,34 @@
+import random
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>123456789"
+    password = ""
+    for i in range(pass_length):
+        password += random.choice(elements)
+    return password
+
+def command_list():
+    commands = """
+/start - Бот отвечает вам "Привет! Я твой Telegram бот. Напиши что-нибудь!"
+
+/hello - Бот отвечает вам "Привет! Как дела?"
+
+/bye - Бот отвечает вам "Пока! Удачи!"
+
+/password - Бот генерирует вам пароль из количества символов ( /password <число> ) 
+
+/help - Команда которая выводит весь список команд
+
+/heh - Выводит столько he сколько вы хотите ( /heh <число> )
+
+/calc - Считает пример за вас ( /calc <пример> )
+
+/newnickname - Добавляет к вашему username символы ( /newnickname <число> )
+
+Если вы ввели команду которой не существует то бот ответит вам тем же что вы написали ему
+    """
+    return commands
+
+def flip():
+    coin = random.randint(1,3)
+    return coin
---
+++ main.py
@@ -0,0 +1,78 @@
+import telebot
+from config import TOKEN
+from logic import gen_pass, command_list, flip
+    
+bot = telebot.TeleBot(TOKEN)
+    
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+    
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+    
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+    
+@bot.message_handler(commands=['password'])
+def send_pass(message):
+    words = message.text.split()
+    if len(words) > 1:
+        try:
+            password = gen_pass(int(words[1]))
+            bot.reply_to(message, f"Ваш пароль: {password}")
+        except (ValueError,SyntaxError,NameError,TypeError):
+            bot.reply_to(message, "Введите число после команды")
+    else:
+        password = gen_pass(8)
+        bot.reply_to(message, password)
+    
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, command_list())
+    
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+    
+@bot.message_handler(commands=['calc'])
+def send_calc(message):
+    to_calc = message.text.split()
+    if len(to_calc) > 1:
+        try:
+            bot.reply_to(message, eval(to_calc[1]))
+        except (SyntaxError, NameError, TypeError):
+            bot.reply_to(message, "Неккоректный пример")
+    
+@bot.message_handler(commands=['newnickname'])
+def send_nickname(message):
+    username = message.from_user.username
+    words = message.text.split()
+    if len(words) > 1:
+        try:
+            password = gen_pass(int(words[1]))
+            bot.reply_to(message, f"Ваш новый ник: {username + password}")
+        except(SyntaxError, TypeError, NameError, ValueError):
+            bot.reply_to(message, "Введите число")
+    else:
+        bot.reply_to(message, "Введите число после команды")
+    
+@bot.message_handler(commands=['flip'])
+def send_flip(message):
+    coin = flip()
+    if coin == 1:
+        bot.reply_to(message, "Выпала решка")
+    elif coin == 2:
+        bot.reply_to(message, "Выпал орёл")
+    elif coin == 3:
+        bot.reply_to(message, "Выпало ребро")
+        
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+    
+bot.polling()
